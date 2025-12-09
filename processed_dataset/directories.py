c086fd2b9eba11f718c3d8c585c4cc45c0b35867
andykim1dev <kyu.andykimkr@gmail.com>
2025-12-08 21:18:17 -0500

    script for splitting images

---
+++ processed_dataset/directories.py
@@ -0,0 +1,16 @@
+"""
+Pathnames for input and output
+"""
+
+# Pathname for initial Augmented Data folder (before running splitfolders)
+INIT_INPUT = '/Users/andykim/Documents/2025 Fall/NNDL/Project/NNDL_MRI_PROJECT/Augmented Data'
+INIT_OUTPUT = '/Users/andykim/Documents/2025 Fall/NNDL/Project/NNDL_MRI_PROJECT/Split Augmented Data'
+
+# Pathname for MRI scan images in train, validation, and test sets
+TEST = '/Users/andykim/Documents/2025 Fall/NNDL/Project/NNDL_MRI_PROJECT/Split Augmented Data/test'
+VAL = '/Users/andykim/Documents/2025 Fall/NNDL/Project/NNDL_MRI_PROJECT/Split Augmented Data/val'
+TRAIN = '/Users/andykim/Documents/2025 Fall/NNDL/Project/NNDL_MRI_PROJECT/Split Augmented Data/train'
+
+
+
+ 
--- processed_dataset/parse_images.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np 
-import argparse 
-from typing import List, Set, Tuple
-import json
-
-def import
-
